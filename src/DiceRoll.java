51ad9767194b542dabf5f12590fd0ee5e10ea1e0
sidkhatri <sidkhatri@gmail.com>
2017-10-27 12:06:35 -0400

    second commit

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -6,63 +6,52 @@ public class DiceRoll {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		//Declare all of my variables
-		int sides;
-		String choice = "y"; 
-		
+
+		String choice = "y";
+
 		System.out.println("Welcome to the Grand Circus Casino!");
 		System.out.println("");
-		
-		while(choice.equalsIgnoreCase("Y")) 
-		{
-			
+
+		while (choice.equalsIgnoreCase("Y")) {
+
 			System.out.println("How many sides should each die have?");
-			sides = scan.nextInt();
-			int sideOne = rollDiceOne(sides); //Calling method 1 and putting value into sideOne
-			int sideTwo = rollDiceTwo(sides); //Calling method 2 and putting value into sideTwo
-		
-			//Printing all results to the console  
+			rollDiceOne(); // Calling method
+
 			System.out.println("");
-			System.out.println("Roll 1:");
-			System.out.println (sideOne);
-			System.out.println (sideTwo);
-			
-		  //Determining if dice reads Snake Eyes, Craps or Box cars and printing the appropriate outcome to the console.	
-			if((sideOne + sideTwo) == 2) {
-				System.out.println("Snake eyes!");
-			} else if((sideOne + sideTwo) == 7) {
-				System.out.println("Craps!"); 
-			} else if((sideOne + sideTwo) == 12) {
-				System.out.println("Box cars!");
-			}
-			
-			scan.nextLine();
-			System.out.println("");
-			System.out.println("");
-			System.out.println("Roll again? (y/n)"); 
+			System.out.println("Roll again? (y/n)");
 			choice = scan.nextLine();
 		}
+		System.out.println("");
 		System.out.println("Thank you for playing at the Grand Circus Casino!");
-		scan.close();	
-		
+		scan.close();
 	}
-	
-	//Method for generating random numbers for Dice One
-	public static int rollDiceOne(int diceFaceOne) {
+
+	// Method for generating random numbers for both Dice
+	public static int rollDiceOne() {
 		Random randGen = new Random();
-		
-		diceFaceOne = (randGen.nextInt(6) + 1);  //This ensures numbers are from 1-6; (otherwise it'd be from 0-5)
-		
+		int userInput;
+		int diceFaceTwo;
+		int diceFaceOne;
+
+		Scanner scan = new Scanner(System.in);
+
+		userInput = scan.nextInt();
+		System.out.println("Roll 1:");
+		diceFaceOne = (randGen.nextInt(userInput) + 1);
+		diceFaceTwo = (randGen.nextInt(userInput) + 1);
+		System.out.println(diceFaceOne + "  " + diceFaceTwo);
+
+		// Determining if dice reads Snake Eyes, Craps or Box cars and printing the
+		// appropriate outcome to the console.
+		if ((diceFaceOne + diceFaceTwo) == 2) {
+			System.out.println("Snake eyes!");
+		} else if ((diceFaceOne + diceFaceTwo) == 7) {
+			System.out.println("Craps!");
+		} else if ((diceFaceOne + diceFaceTwo) == 12) {
+			System.out.println("Box cars!");
+		}
+
 		return diceFaceOne;
 	}
-	
-	//Method for generating random numbers for Dice Two
-	public static int rollDiceTwo(int diceFaceTwo) {
-		Random randGen = new Random();
-		
-		diceFaceTwo = (randGen.nextInt(6) + 1); 
-		
-		return diceFaceTwo;
-	}
+
 }
